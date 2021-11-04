6cfc2e63e94206cd8e302498d5f15118c397c982
Alexander Messier <alexander.j.messier@gmail.com>
2021-11-04 16:01:17 -0500

    Removed Member model and made user a ManyToManyField in class Listing. Made migrations.

--- drf_jwt_capstone_backend/paintball/views.py
+++ drf_jwt_capstone_backend/paintball/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from .models import Listing, Review, Member
-from .serializers import ListingSerializer, ReviewSerializer, MemberSerializer
+from .models import Listing, Review
+from .serializers import ListingSerializer, ReviewSerializer
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -97,21 +97,21 @@ def get_all_reviews(request):
 # Member queries
 # Post member AKA join listing
 # Gets members
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_members(request, user_id, listers_id):
-    members = Member.get.all(listing_id=listers_id)
-    member_users = members.get.all()
-    serializer = MemberSerializer(members, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def get_members(request, user_id, listers_id):
+#     members = Member.get.all(listing_id=listers_id)
+#     member_users = members.get.all()
+#     serializer = MemberSerializer(members, many=True)
+#     return Response(serializer.data)
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def post_member(request):
-    if request.method == 'POST':
-        serializer = MemberSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def post_member(request):
+#     if request.method == 'POST':
+#         serializer = MemberSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save(user=request.user)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
