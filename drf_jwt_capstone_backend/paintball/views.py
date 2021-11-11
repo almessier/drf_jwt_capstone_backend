4b3d14add30e0f3565dab93dae2b74644e4814f5
Alexander Messier <alexander.j.messier@gmail.com>
2021-11-10 20:51:31 -0600

    Removed all arguments in seralizer.save() inside of post_review() view.

--- drf_jwt_capstone_backend/paintball/views.py
+++ drf_jwt_capstone_backend/paintball/views.py
@@ -81,7 +81,7 @@ def post_review(request):
     if request.method == 'POST':
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
